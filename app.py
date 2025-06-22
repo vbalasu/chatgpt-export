f696fcf4865256bf610d5e24dbcc4cc1555f998f
Vijay Balasubramaniam <vbalasu@gmail.com>
2025-06-22 10:00:40 -0700

    Added instructions and logged url

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ from export import export_pdf
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return 'hello world'
+    return '<h1>USAGE</h1><p>https://chatgpt-export.cloudmatica.com/pdf?url=<strong>https://chatgpt.com/share/YOUR_CHATGPT_SHARE_CODE</strong></p>'
 
 @app.route('/pdf')
 def export_to_pdf():
     url = request.args.get('url')
+    print(url)
     export_pdf(url)
     return send_file('page.pdf', as_attachment=True)
